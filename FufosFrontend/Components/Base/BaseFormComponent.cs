fcc42cf89129bc2ad21be16a83a47f84e209dcd8
PinedaBrayan <brayan.pineda@cecar.edu.co>
2024-06-05 21:43:27 -0500

    Componentes

---
+++ FufosFrontend/Components/Base/BaseFormComponent.cs
@@ -0,0 +1,26 @@
+
+using Fufos.Globals.Enums;
+using FufosFrontend.Crud;
+using FufosFrontend.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace FufosFrontend.Components.Base;
+
+public abstract class BaseFormComponent<T> : ComponentBase
+{
+    [Inject] public TranslatorService TranslatorService { get; set; } = null!;
+    [CascadingParameter] public ViewType ViewType { get; set; }
+    [CascadingParameter] public DynamicBaseView View {get; set;} = null!;
+    protected T Entity {get; set;} = default!;
+
+    protected void InitEntity()
+    {
+        Entity = (T) View.Entity;
+    }
+
+    protected override void OnInitialized()
+    {
+        InitEntity();
+        base.OnInitialized();
+    }
+}
